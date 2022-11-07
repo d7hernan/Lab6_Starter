05e88fb67f513e7a22033605dc4dc1f0d9e80b39
d7hernan <83631798+d7hernan@users.noreply.github.com>
2022-11-06 20:58:45 -0800

    Expose complete

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -11,6 +11,7 @@ class RecipeCard extends HTMLElement {
     const shadow = this.attachShadow({ mode: "open" });
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
     const article = document.createElement("article");
+    //const article_shadow = article.attachShadow({ mode: "open" }); 
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
     let style = document.createElement("style");
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
@@ -124,12 +125,29 @@ class RecipeCard extends HTMLElement {
     if (!data) return;
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
-    const Myarticle = shadow.article;
+    const Myarticle = this.shadowRoot.querySelector('article');
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
     //           literals (tempalte strings) and element.innerHTML for this.
-    Myarticle.setAttribute(); 
+    Myarticle.innerHTML = `
+      <img src=${data["imgSrc"]}
+        alt="${data["imgAlt"]}>
+      <p class="title>
+        <a href=${data["titleLnk"]}>${data["titleTxt"]}</a>
+      </p>
+      <p class="organization">${data["organization"]}</p>
+        <div class="rating">
+          <span>${data["rating"]}</span>
+          <img src="/assets/images/icons/${data["rating"]}-star.svg" alt="${data["rating"]} stars">
+          <span>(${data["numRatings"]})</span>
+        </div>
+        <time>${data["lengthTime"]}</time>
+      <p class="ingredients">
+       ${data["ingredients"]}
+      </p>
+    
+    `; 
   }
 }
 
